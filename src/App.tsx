3815bf15f425aca89369ac35f9bc1d1be3f93ee8
Isa Muhammed Alhaji <isamuhammad0131@gmail.com>
2025-05-14 22:27:48 +0100

    chore: playground 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,34 @@
 import { LoginOutlined } from "@ant-design/icons";
-import { Button, Flex, Input, Typography } from "antd";
+import { Button, Col, Flex, Input, Row, Typography } from "antd";
 // import "./App.css";
 
 function App() {
-
   // display -> flex, grid, inline-flex, block, inline-block
   // padding -> TROBLE -> Top, Right, bottom, left -> spacing between container and it's content
   // Margin -> TROUBLE -> Top, Right, bottom, left -> spacing
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        height: "100vh",
-        gap: "20px",
-      }}
-    >
-      <Flex vertical gap={20} style={{ width: "25%", margin: "auto auto" }}>
-        <Typography.Title level={3}>Login</Typography.Title>
-        <Input placeholder="username" />
+    <Flex vertical style={{ height: "100vh" }}>
+      <Row>
+        <Col
+          xs={24}
+          sm={24}
+          md={8}
+          style={{ height: "100vh", margin: "auto 0", padding: "20px" }}
+        >
+          <Flex vertical gap={20}>
+            <Input placeholder="username" />
 
-        <Input placeholder="password" />
+            <Input placeholder="Password" />
 
-        <Flex justify="space-between">
-          <Typography.Link>Forgot Password</Typography.Link>
-          <Button
-            type="primary"
-            icon={<LoginOutlined />}
-            style={{ alignSelf: "end" }}
-          >
-            Login
-          </Button>
-        </Flex>
-      </Flex>
-    </div>
+            <Flex justify="space-between">
+              <Typography.Link>Forgot Password</Typography.Link>
+              <Button type="primary" icon={<LoginOutlined />}>Login</Button>
+            </Flex>
+          </Flex>
+        </Col>
+      </Row>
+    </Flex>
   );
 }
 
